d669149b3a9524d9486dc00e4322ad5b7cd45934
Gonzalo Blanco Mosteiro <gblamos249@g.educaand.es>
2026-01-16 13:18:01 +0100

    Ejercicios 4.7, 4.8, 4.9

--- src/main/kotlin/org/iesra/Main.kt
+++ src/main/kotlin/org/iesra/Main.kt
@@ -1,16 +1,104 @@
 package org.iesra
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.time.LocalDateTime
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+// Ejercicio 4.7
+class Cuenta(val cuenta: Int, var saldo: Double)
+{
+    fun abono(cantidad: Double, cuenta: Cuenta)
+    {
+        this.saldo -= cantidad
+        cuenta.saldo += cantidad
     }
+
+    fun pago(cantidad: Double, cuenta: Cuenta)
+    {
+        this.saldo -= cantidad
+        cuenta.saldo += cantidad
+    }
+
+    companion object {
+        fun esMorosa(cuenta: Persona): Boolean
+        {
+            for (i in cuenta.cuentas) { if (i.saldo < 0) return true }
+            return false
+        }
+
+        fun transferencia(persona_origen: Persona, persona_destino: Persona, cuenta_origen: Int, cuenta_destino: Int, cantidad: Double): Boolean
+        {
+            fun encontrarCuenta(persona: Persona, cuenta: Int): Int
+            { return persona.cuentas.indexOfFirst() { it.cuenta == cuenta } }
+
+            try {
+                persona_origen.cuentas[encontrarCuenta(persona_origen, cuenta_origen)]
+                    .pago(cantidad, persona_destino.cuentas[encontrarCuenta(persona_destino, cuenta_destino)])
+            } catch (error: Exception) { return false }
+
+            return true
+        }
+    }
+}
+
+class Persona(var dni: String, var cuentas: ArrayList<Cuenta>)
+{
+    fun addCuenta(cuenta: Cuenta) { if (cuentas.size > 3) cuentas.add(cuenta) }
+}
+
+// Ejercicio 4.8
+class Libro(var titulo: String, var autor: String, var numPags: Int, var calificacion: Int)
+{
+    init {
+        if (calificacion !in 0..10) throw Exception("$calificacion debe de estar entre 0 y 10")
+    }
+}
+
+class ConjuntoLibros(var conjuntoLibros: ArrayList<Libro>)
+{
+    fun addLibro(libro: Libro) { if (conjuntoLibros.size < 3) conjuntoLibros.add(libro) }
+
+    fun eliminarAutor(libro: Libro, autor: String)
+    { conjuntoLibros.remove(conjuntoLibros[conjuntoLibros.indexOfFirst() { it.autor == autor }]) }
+
+    fun eliminarTitulo(libro: Libro, titulo: String)
+    { conjuntoLibros.remove(conjuntoLibros[conjuntoLibros.indexOfFirst() { it.titulo == titulo }]) }
+
+    override fun toString(): String { return "ConjuntoLibros($conjuntoLibros)" }
+}
+
+enum class TareaEstado { PENDIENTE, COMPLETADA }
+
+// Ejercicio 4.9
+class Tarea(var descripcion: String, var estado: TareaEstado? = TareaEstado.PENDIENTE)
+{
+    var id: Int = 0
+    var fecha: LocalDateTime? = null
+
+    init { id++ }
+}
+
+class conjuntoTareas(var conjuntoTareas: ArrayList<Tarea>)
+{
+    fun addTarea(tarea: Tarea) { conjuntoTareas.add(tarea) }
+
+    fun cambiarEstado(id: Int, estado: TareaEstado)
+    {
+        val tarea: Tarea = conjuntoTareas[conjuntoTareas.indexOfFirst() { it.id == id }]
+        if (estado == TareaEstado.COMPLETADA) tarea.fecha = LocalDateTime.now()
+        tarea.estado = estado
+    }
+
+    fun eliminarEstado(id: Int)
+    {
+        val tarea: Tarea = conjuntoTareas[conjuntoTareas.indexOfFirst() { it.id == id }]
+        tarea.estado = null
+        tarea.fecha = null
+    }
+}
+
+
+
+fun main()
+{
+
 }
