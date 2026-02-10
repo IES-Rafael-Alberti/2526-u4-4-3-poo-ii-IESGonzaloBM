fef9e062e0a1fa08f65c76b72576ec856affa6ee
Gonzalo Blanco Mosteiro <gblamos249@g.educaand.es>
2026-02-10 12:28:35 +0100

    5.2.a Uso de herencia (a,b,c,d,e,f,h) Terminado

--- src/main/kotlin/org/iesra/Main.kt
+++ src/main/kotlin/org/iesra/Main.kt
@@ -1,189 +1,54 @@
 package org.iesra
 
-import java.time.LocalDateTime
-import org.slf4j.LoggerFactory
+import kotlin.random.Random
 
-// Ejercicio 4.6
-/**
- * Clase Compra
- * @param cliente cliente que realizo la compra
- * @param dia dia de la compra
- * @param monto monto de la compra
- * @constructor Crea una compra con cliente, dia y monto
- */
-data class Compra(var cliente: Cliente, var dia: Int, var monto: Int)
+enum class RADIO { PEQUENO, AMPLIO }
+
+open class ArmaDeFuego(private var nombre: String,
+                  private var municion: Int,
+                  private var municionARestar: Int,
+                  private var tipoDeMunicion: String,
+                  private var danio: Int,
+                  private var radio: RADIO)
 {
+    fun estado(): String { return "$nombre(municion: $municion, municionARestar: $municionARestar, tipoDeMunicion: $tipoDeMunicion, danio: $danio, radio: $radio)" }
 
-}
+    open fun disparar() { municion -= municionARestar }
 
-/**
- * Clase Cliente
- * @param nombre nombre del cliente
- * @param domicilio domicilio del cliente
- * @constructor Crea un cliente con nombre y domicilio
- */
-data class Cliente(var nombre: String, var domicilio: Domicilio)
-{
-
+    open fun recargar() { municion++ }
 }
 
 
-/**
- * Clase Domicilio
- * @param calle calle del domicilio
- * @param numero numero del domicilio
- * @constructor Crea un domicilio con calle y numero
- */
-data class Domicilio(var calle: String, var numero: Int)
+class Pistola(nombre: String, municion: Int, municionARestar: Int, tipoDeMunicion: String, danio: Int, radio: RADIO)
+    : ArmaDeFuego(nombre, municion, municionARestar, tipoDeMunicion, danio, radio)
+
+
+class Rifle(nombre: String, municion: Int, municionARestar: Int, tipoDeMunicion: String, danio: Int, radio: RADIO)
+    : ArmaDeFuego(nombre, municion, municionARestar, tipoDeMunicion, danio, radio)
 {
-    fun dirCompleta(): String
-    { return "Calle: $calle, num: $numero" }
+    override fun disparar() { municion -= municionARestar * 2}
+
 }
 
-/**
- * Clase RepositorioCompras
- * @constructor Crea un repositorio de compras
- */
-data class RepositorioCompras()
-{
-    fun agregarCompra()
-    {
 
+class Bazooka(nombre: String, municion: Int, municionARestar: Int, tipoDeMunicion: String, danio: Int, radio: RADIO)
+    : ArmaDeFuego(nombre, municion, municionARestar, tipoDeMunicion, danio, radio)
+{
+    override fun disparar() { municion -= municionARestar * 3}
+}
+
+
+
+fun main()
+{
+    val pistola = Pistola("MiPistola", 100, 3, "Balas", 5, RADIO.PEQUENO)
+    val rifle = Rifle("MiRifle", 25, 2, "BalasGrandes", 10, RADIO.AMPLIO)
+    val bazooka = Bazooka("MiBazooka", 5, 1, "Misil", 30, RADIO.AMPLIO)
+
+    val armas = mapOf<Int, ArmaDeFuego>(1 to pistola, 2 to rifle, 3 to bazooka)
+
+    armas.forEach { K, V ->
+        repeat(6) { V.disparar() }
+        V.estado()
     }
-
-    fun domicilios()
-    {
-
-    }
-}
-
-
-// Ejercicio 4.7
-class Cuenta(val cuenta: Int, var saldo: Double)
-{
-    fun abono(cantidad: Double, cuenta: Cuenta)
-    {
-        this.saldo -= cantidad
-        cuenta.saldo += cantidad
-    }
-
-    fun pago(cantidad: Double, cuenta: Cuenta)
-    {
-        this.saldo -= cantidad
-        cuenta.saldo += cantidad
-    }
-
-    companion object {
-        fun esMorosa(cuenta: Persona): Boolean
-        { return cuenta.cuentas.any { (it.saldo ?: 0.0) < 0.0 } }
-
-        fun transferencia(persona_origen: Persona, persona_destino: Persona, cuenta_origen: Int, cuenta_destino: Int, cantidad: Double): Boolean
-        {
-            fun encontrarCuenta(persona: Persona, cuenta: Int): Int
-            { return persona.cuentas.indexOfFirst() { it.cuenta == cuenta } }
-
-            try {
-                persona_origen.cuentas[encontrarCuenta(persona_origen, cuenta_origen)]
-                    .pago(cantidad, persona_destino.cuentas[encontrarCuenta(persona_destino, cuenta_destino)])
-            } catch (error: Exception) { return false }
-
-            return true
-        }
-    }
-}
-
-class Persona(var dni: String, var cuentas: ArrayList<Cuenta>)
-{
-    fun addCuenta(cuenta: Cuenta) { if (cuentas.size > 3) cuentas.add(cuenta) }
-}
-
-// Ejercicio 4.8
-class Libro(var titulo: String, var autor: String, var numPags: Int, var calificacion: Int)
-{
-    init {
-        if (calificacion !in 0..10) throw Exception("$calificacion debe de estar entre 0 y 10")
-    }
-}
-
-class ConjuntoLibros(var conjuntoLibros: ArrayList<Libro>)
-{
-    fun addLibro(libro: Libro) { if (conjuntoLibros.size < 3) conjuntoLibros.add(libro) }
-
-    fun eliminarAutor(libro: Libro, autor: String)
-    { conjuntoLibros.remove(conjuntoLibros[conjuntoLibros.indexOfFirst() { it.autor == autor }]) }
-
-    fun eliminarTitulo(libro: Libro, titulo: String)
-    { conjuntoLibros.remove(conjuntoLibros[conjuntoLibros.indexOfFirst() { it.titulo == titulo }]) }
-
-    override fun toString(): String { return "ConjuntoLibros($conjuntoLibros)" }
-}
-
-enum class TareaEstado { PENDIENTE, COMPLETADA }
-
-// Ejercicio 4.9
-class Tarea(var descripcion: String, var estado: TareaEstado? = TareaEstado.PENDIENTE)
-{
-    var id: Int = 0
-    var fecha: LocalDateTime? = null
-
-    init { id++ }
-}
-
-class conjuntoTareas(var conjuntoTareas: ArrayList<Tarea>)
-{
-    fun addTarea(tarea: Tarea) { conjuntoTareas.add(tarea) }
-
-    fun cambiarEstado(id: Int, estado: TareaEstado)
-    {
-        val tarea: Tarea = conjuntoTareas[conjuntoTareas.indexOfFirst() { it.id == id }]
-        if (estado == TareaEstado.COMPLETADA) tarea.fecha = LocalDateTime.now()
-        tarea.estado = estado
-    }
-
-    fun eliminarEstado(id: Int)
-    {
-        val tarea: Tarea = conjuntoTareas[conjuntoTareas.indexOfFirst() { it.id == id }]
-        tarea.estado = null
-        tarea.fecha = null
-    }
-}
-
-// Ejercicio 4.10
-//class Tablero(val dimension: Int, val fichas: ArrayList<Ficha>)
-//{
-//    private val tablero = Array(dimension) { Array(dimension) { "O" } }
-//    private var started: Boolean = false
-//
-//    fun startGame()
-//    {
-//        started = true
-//        while (started) {
-//            val fichasA: Array<Ficha> = [Ficha(), Ficha(), Ficha()]
-//            val fichasB: Array<Ficha> = [Ficha(), Ficha(), Ficha()]
-//        }
-//    }
-//
-//    fun viewTablero()
-//    {
-//        for (i in 0..<tablero.size) {
-//            println(tablero[i])
-//        }
-//    }
-//}
-//
-//enum class Color { AZUL, ROJO }
-//class Ficha(color: Color)
-//{
-//
-//}
-
-
-val logger = LoggerFactory.getLogger("Tarea")
-
-fun main() {
-    logger.trace("Mensaje TRACE")
-    logger.debug("Mensaje DEBUG")
-    logger.info("Mensaje INFO")
-    logger.warn("Mensaje WARN")
-    logger.error("Mensaje ERROR")
 }
